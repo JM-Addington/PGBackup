1a5f448043c61da20e72598c8ab91f7cd5716ed2
Jonathan Addington <jonathan.addington@jmaddington.com>
2023-08-08 12:43:59 -0400

    Add clarity to backup script output

--- app/backup.sh
+++ app/backup.sh
@@ -11,7 +11,7 @@ file="$date-$FRIENDLY_NAME-$POSTGRES_DB-backup.sql.gz.gpg"
 #pg_dump "dbname=$POSTGRES_DB user=$POSTGRES_USER host=$POSTGRES_HOST port=$POSTGRES_PORT sslmode=require"
 
 echo "Starting backup process..."
-echo "$FRIENDLY_NAME"
+echo "FRIENDLY_NAME: $FRIENDLY_NAME"
 
 pg_dump -h $POSTGRES_HOST -U $POSTGRES_USER -d $POSTGRES_DB -p $POSTGRES_PORT | \
     pv | \
