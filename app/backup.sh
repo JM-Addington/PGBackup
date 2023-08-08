f108acedddadf20aba1aae53fec3ffd0bd357534
Jonathan Addington <jonathan.addington@jmaddington.com>
2023-08-08 12:18:59 -0400

    Add helpful output during backup

--- app/backup.sh
+++ app/backup.sh
@@ -10,8 +10,13 @@ file="$date-$FRIENDLY_NAME-$POSTGRES_DB-backup.sql.gz.gpg"
 
 #pg_dump "dbname=$POSTGRES_DB user=$POSTGRES_USER host=$POSTGRES_HOST port=$POSTGRES_PORT sslmode=require"
 
+echo "Starting backup process..."
+echo "$FRIENDLY_NAME"
+
 pg_dump -h $POSTGRES_HOST -U $POSTGRES_USER -d $POSTGRES_DB -p $POSTGRES_PORT | \
     pv | \
     pigz -9 | \
     gpg --encrypt --recipient-file ./key \
     > /backup/$file
+
+echo "Backup complete!"
